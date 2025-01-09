c23b2853dbf7e579e310f64700d5113c24543d76
Tiziano <tizianocitro98@gmail.com>
2025-01-09 09:27:45 +0100

    feat: logging conf now is in main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from experiment import run_experiment
+import os
+
+
+def setup(exp_name="experiment"):
+    # set for logging file
+    os.environ["LOG_FILE_DIR"] = "logs"
+    os.environ["LOG_FILE_PATH"] = exp_name
 
 
 if __name__ == "__main__":
@@ -6,5 +13,8 @@ if __name__ == "__main__":
     graph_name = "erdos_renyi_graph"
     cost = 500
     n = 20
+
     for i in range(1):
-        run_experiment(f"testing experiment {i}", epochs, graph_name, cost, n)
+        exp_name = f"testing experiment {i}"
+        setup(exp_name)
+        run_experiment(exp_name, epochs, graph_name, cost, n)
