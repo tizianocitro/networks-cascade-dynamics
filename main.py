39e6c7ef563d86fc2d9967e34200f23e97c03705
Tiziano <tizianocitro98@gmail.com>
2025-01-10 09:35:19 +0100

    feat: params are now available as cli args

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from experiment import run_experiment
+import argparse
 import os
 
 
@@ -9,12 +10,23 @@ def setup(exp_name="experiment"):
 
 
 if __name__ == "__main__":
-    epochs = 50
-    graph_name = "erdos_renyi_graph"
-    cost = 500
-    n = 20
+    parser = argparse.ArgumentParser(description="Run experiments.")
+    parser.add_argument("-g", "--graph_name", type=str, default="erdos_renyi_graph", help="Graph name")
+    parser.add_argument("-c", "--cost", type=int, default=500, help="Cost value")
+    parser.add_argument("-n", "--set_size", type=int, default=20, help="Size of seed set")
+    parser.add_argument("-e", "--epochs", type=int, default=50, help="Number of epochs")
+    parser.add_argument("-r", "--runs", type=int, default=1, help="Number of experiments to run")
+    parser.add_argument("-exp", "--experiment_name", type=str, default="Experiment", help="Experiment name")
+    args = parser.parse_args()
 
-    for i in range(1):
-        exp_name = f"testing experiment {i}"
-        setup(exp_name)
-        run_experiment(exp_name, epochs, graph_name, cost, n)
+    graph_name = args.graph_name
+    cost = args.cost
+    n = args.set_size
+    epochs = args.epochs
+    runs = args.runs
+    exp_name = args.experiment_name
+
+    for i in range(runs):
+        run_name = f"{exp_name} {i}"
+        setup(run_name)
+        run_experiment(run_name, epochs, graph_name, cost, n)
